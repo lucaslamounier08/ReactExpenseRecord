c5a05c906e8721cb2caefee4cfc036729f0364f0
lucas.faria <lucas.faria@ambevtech.com.br>
2023-01-16 18:58:24 -0300

    refactoring application

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const  App = () => {
   const expenses = [
     { title: "Car Insurance", amount: 294.67, date: new Date(2023, 2, 12) },
     { title: "Car Insurance 2", amount: 912.85, date: new Date(2021, 2, 12) },
@@ -9,16 +9,7 @@ function App() {
   return (
     <div>
       <h2>Teste do Farco</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
 }
