5b2904a95b1162e6e601b9bc3771fd822454e29e
lucas.faria <lucas.faria@ambevtech.com.br>
2023-01-16 19:31:36 -0300

    added button to change title name and github homepage

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,30 @@
+import React, { useState } from "react";
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  let date = props.date;
+  let amount = props.amount;
+
+  const clickHandler = () => {
+    setTitle("hey from old value: " + props.title);
+    console.log("Clicked");
+  };
+
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date} />
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
-      <button>Change title!</button>
+      <button onClick={clickHandler}>Change title!</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
