e414d423071ed04b8204f0099ade2e863c3b603f
lucas.faria <lucas.faria@ambevtech.com.br>
2023-02-08 19:17:14 -0300

    Adding expense form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
+import React from "react";
+
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
-const  App = () => {
+const App = () => {
   const expenses = [
-    { title: "Car Insurance", amount: 294.67, date: new Date(2023, 2, 12) },
+    { title: "Car Insurance 1", amount: 294.67, date: new Date(2023, 2, 12) },
     { title: "Car Insurance 2", amount: 912.85, date: new Date(2021, 2, 12) },
   ];
 
   return (
     <div>
-      <h2>Teste do Farco</h2>
+      <h2>Learning React</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
