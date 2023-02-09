1e6eb52baeba0765677b996e4b31fa7bfbe95d56
lucas.faria <lucas.faria@ambevtech.com.br>
2023-02-09 18:09:37 -0300

    Added ExpensesList to clean code

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
+import ExpensesList from "./ExpensesList";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2023");
@@ -16,19 +16,6 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <Card className="expenses">
       <ExpensesFilter
@@ -36,7 +23,7 @@ function Expenses(props) {
         onChangeFilter={filterChangeHandler}
       />
 
-      {expensesContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 }
